0edcfdaee423ee6c81bcfd526f981a0d326de348
vibhavdubey <vibhavkumardubey19@gmail.com>
2024-11-14 21:14:41 +0530

    added speciality menu and top doctors components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,21 @@ const Header = () => {
         <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
           Book Appointment <br /> With Trusted Doctors
         </p>
-        <div>
-          <img src={assets.group_profiles} alt="" />
+        <div className="flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light">
+          <img className="w-28" src={assets.group_profiles} alt="" />
           <p>
             Simply browse through our extensive list of trusted doctors,
-            <br />
+            <br className="hidden sm:block" />
             schedule your appointment hassle-free.
           </p>
-          <a href="">
-            Book appointment <img src={assets.arrow_icon} alt="" />
-          </a>
         </div>
+        <a
+          href="#speciality"
+          className="flex items-center gap-2 bg-white px-8 py-2 rounded-full text-gray-600  text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
+        >
+          Book appointment{" "}
+          <img src={assets.arrow_icon} className="w-3" alt="" />
+        </a>
       </div>
       {/* right side */}
       <div className="md:w-1/2 relative">
